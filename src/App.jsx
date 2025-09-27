4984360b3dfce51a9f8ec33c51395b806ef64ebe
Nadim Mahmud <nadeemmaahmud@gmail.com>
2025-09-27 17:43:05 +0600

    hash router configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Books from './components/Books';
 import NewBook from './components/NewBook';
 import EditBook from './components/EditBook';
@@ -10,14 +10,14 @@ const App = () => {
 
   return (
     <div className='bg-all'>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Books api_url={api_url}/>}/>
           <Route path='/add' element={<NewBook api_url={api_url}/>}/>
           <Route path='/edit/:id' element={<EditBook api_url={api_url}/>}/>
           <Route path='*' element={<NotFound/>}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
